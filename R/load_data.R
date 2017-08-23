30e335e8e6e0ffc932ae3abdbd870c9ab3ac35d1
Laurent Modolo <laurent.modolo@ens-lyon.fr>
2017-08-23 17:55:45 +0200

    load_data.R: scdata R6 object add method for selecting and loading object

--- R/load_data.R
+++ R/load_data.R
@@ -10,7 +10,33 @@ scdata <- R6::R6Class("scdata",
     genes = NULL,
     cells = NULL,
     counts = NULL,
-    features = NULL
+    features = NULL,
+    get_rc_counts = function(cells = NULL, genes = NULL) {
+      c_num <- 1:length(private$genes)
+      if (!is.null(genes)) {
+        c_num <- which(private$genes %in% genes)
+      }
+      r_num <- 1:length(private$cells)
+      if (!is.null(cells)) {
+        r_num <- which(private$cells %in% cells)
+      }
+      cat(paste0("ncol: ", length(c_num), ".\n"))
+      cat(paste0("nrow: ", length(r_num), ".\n"))
+      return(list(c_num = c_num, r_num = r_num))
+    },
+    get_rc_features = function(cells = NULL, features = NULL) {
+      c_num <- 1:length(private$features)
+      if (!is.null(features)) {
+        c_num <- which(colnames(private$features) %in% features)
+      }
+      r_num <- 1:length(private$cells)
+      if (!is.null(cells)) {
+        r_num <- which(private$cells %in% cells)
+      }
+      cat(paste0("ncol: ", length(c_num), ".\n"))
+      cat(paste0("nrow: ", length(r_num), ".\n"))
+      return(list(c_num = c_num, r_num = r_num))
+    }
     ),
   public = list(
     initialize = function(infos = NA, counts = NA) {
@@ -24,11 +50,43 @@ scdata <- R6::R6Class("scdata",
       cat(paste0("ncol: ", ncol(private$counts), ".\n"))
       cat(paste0("nrow: ", nrow(private$counts), ".\n"))
       cat(paste0("features: ", ncol(private$features), ".\n"))
+    },
+    getfeature = function(feature) {
+      return(private$features[[feature]])
+    },
+    getgene = function(gene) {
+      c_num <- which(private$genes %in% gene)
+      return(private$counts[, c_num])
+    },
+    getcountsw = function(cells = NULL, genes = NULL) {
+      rc_num <- private$get_rc_counts(cells = cells, genes = genes)
+      return(private$counts[rc_num$r_num, rc_num$c_num])
+    },
+    getfeaturesw = function(cells = NULL, features = NULL) {
+      rc_num <- private$get_rc_features(cells = cells, features = features)
+      return(private$features[rc_num$r_num, rc_num$c_num])
+    },
+    getcountso = function(cells = NULL, genes = NULL) {
+      rc_num <- private$get_rc_counts(cells = cells, genes = genes)
+      return(private$counts[-rc_num$r_num, -rc_num$c_num])
+    },
+    getfeatureso= function(cells = NULL, features = NULL) {
+      rc_num <- private$get_rc_features(cells = cells, features = features)
+      return(private$features[-rc_num$r_num, -rc_num$c_num])
     }
   ),
   active = list(
     getcounts = function() {
       return(private$counts)
+    },
+    getfeatures = function() {
+      return(private$features)
+    },
+    getcells = function() {
+      return(private$cells)
+    },
+    getgenes = function() {
+      return(private$genes)
     }
   )
 )
