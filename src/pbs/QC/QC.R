ec5afa51ae89dda2e52cda5fbf0068ea3eff70a0
Laurent Modolo <laurent.modolo@ens-lyon.fr>
2017-09-21 17:57:46 +0200

    QC.R: force reload of scRNAtools

--- src/pbs/QC/QC.R
+++ src/pbs/QC/QC.R
@@ -1,6 +1,7 @@
 #!/usr/bin/Rscript
 library(scRNAtools)
-QC_pbs(
+devtools::load_all("../scRNAtools/", reset = T)
+scRNAtools::QC_pbs(
   scd_file = "results/raw_counts.Rdata",
   QC_folder = "results/QC_paraload/"
 )
