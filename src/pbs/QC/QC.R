e9788fc81711a51743f08939c3360e398c35f77e
Laurent Modolo <laurent.modolo@ens-lyon.fr>
2017-09-22 15:12:30 +0200

    QC.R: fix results path

--- src/pbs/QC/QC.R
+++ src/pbs/QC/QC.R
@@ -3,5 +3,5 @@ library(scRNAtools)
 devtools::load_all("../scRNAtools/", reset = T)
 scRNAtools::QC_pbs(
   scd_file = "results/raw_counts.Rdata",
-  QC_folder = "results/QC_paraload/"
+  QC_folder = "results/QC/QC_paraload/"
 )
