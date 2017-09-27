016b60be6f017d12a75fe5edd1974452c033aa69
Laurent Modolo <laurent.modolo@ens-lyon.fr>
2017-09-27 11:38:06 +0200

    base.R: add anscombe transform

--- R/base.R
+++ R/base.R
@@ -23,3 +23,44 @@ get_files <- function(path, regexp) {
   file_list <- file_list[grepl(regexp, file_list, perl = T)]
   return(file_list)
 }
+
+#' anscombe transform of counts
+#'
+#' @param x vector of counrs
+#' @return return anscomb transform of x
+#' @examples
+#' \dontrun{
+#' x = anscombe(scd$getgene("gene_a"))
+#' }
+#' @export anscombe
+anscombe <- function(x){
+  return(2 * sqrt(x + 3 / 8))
+}
+
+#' anscombe transform of counts
+#'
+#' @param x vector of counrs
+#' @param to_zero (bool default: TRUE) set ascb(x) to 0 when x = 0
+#' @param na.rm remove NA values
+#' @return return anscomb transform of x
+#' @examples
+#' \dontrun{
+#' x = ascb(scd$getgene("gene_a"))
+#' }
+#' @export ascb
+ascb <- function(x, to_zero=TRUE, na.rm=FALSE){
+  if (na.rm){
+    if (!is.null(dim(x))){
+      x <- x[, apply(x, 2, function(x){
+        !any(is.na(x))
+      })]
+    }
+  }
+  if (to_zero){
+    if (min(x, na.rm = T) < 0){
+      x[!is.na(x)] <- x[!is.na(x)] - min(x, na.rm = TRUE)
+    }
+    return(anscombe(x) - anscombe(0))
+  }
+  return(anscombe(x))
+}
