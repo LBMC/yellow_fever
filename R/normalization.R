3d8e05724635eb708ee1f60a282d4c197837104a
Laurent Modolo <laurent.modolo@ens-lyon.fr>
2017-11-17 12:05:35 +0100

    normalization.R : fix normalization caller function

--- R/normalization.R
+++ R/normalization.R
@@ -18,7 +18,7 @@ normalize <- function(
     tmp_file,
     v = F
   ) {
-  algo_norm <- paste0(algo, "_normalize")
+  algo_norm <- paste0(method, "_normalize")
   return(algo_norm(
     scd = scd,
     b_cells = b_cells,
