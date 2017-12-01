c184dbe1efa2495c477117cf7b0f2b13406071bf
Laurent Modolo <laurent.modolo@ens-lyon.fr>
2017-12-01 18:00:10 +0100

    differential_expression.R: fixed vugon test

--- R/differential_expression.R
+++ R/differential_expression.R
@@ -291,30 +291,26 @@ formula_to_features <- function(scd, formula_full){
 
 #' @importFrom MASS glm.nb
 #' @importFrom pscl zeroinfl
-#' @importFROM pscl vuong
 zi_test <- function(data, formula_full, gene_name,
     family = "nbinom1", link = "log", threshold = 0.05, v = F){
-  if(max(data$y) == 0){
+  if (max(data$y) == 0){
     stop(paste0("error : ", gene_name, " contains only zeros"))
   }
-  m1 <- ziNB_fit(
-    data = data,
-    formula = formula_full,
-    gene_name = gene_name,
-    family = family,
-    link = link,
-    zi = FALSE,
-    v = v
-  )
-  m2 <- ziNB_fit(
-    data = data,
-    formula = formula_full,
-    gene_name = gene_name,
-    family = family,
-    link = link,
-    zi = TRUE,
-    v = v
-  )
+  m1 <- tryCatch({
+      glm.nb(y ~ 1,
+             data = data)
+    }, error = function(e){
+      print("error in NB model")
+      return(list(residuals = NA))
+    })
+  m2 <- tryCatch({
+      zeroinfl(y ~ 1,
+               data = data,
+               dist = "negbin")
+    }, error = function(e){
+      print("error in ziNB model")
+      return(list(residuals = NA))
+    })
   if (is.na(m1$residuals[1]) & is.na(m2$residuals[1])) {
     if(sum(data$y == 0) < 3){
       if (v) {
@@ -349,7 +345,7 @@ zi_test <- function(data, formula_full, gene_name,
     return(FALSE)
   }
   test <- vuong_test(m1 = m1, m2 = m2, v = T)
-  if(test < threshold){
+  if (test < threshold){
     if (v) {
       print(paste0("zeroinfl detected for ", gene_name, " (vuong test)"))
     }
@@ -361,92 +357,74 @@ zi_test <- function(data, formula_full, gene_name,
   return(FALSE)
 }
 
-vuong_test <- function (m1, m2, digits = getOption("digits"), v){
-  m1y <- m1$frame$y
-  m2y <- m2$frame$y
-  m1n <- length(m1y)
-  m2n <- length(m2y)
-  if (m1n == 0 | m2n == 0)
-    stop("Could not extract dependent variables from models.")
-  if (m1n != m2n)
-    stop(paste("Models appear to have different numbers of observations.\n",
-        "Model 1 has ", m1n, " observations.\n", "Model 2 has ",
-        m2n, " observations.\n", sep = ""))
-  if (any(m1y != m2y))
-    stop(paste("Models appear to have different values on dependent variables.\n"))
-  p1 <- predprob(m1, zi = FALSE)
-  p2 <- predprob(m2, zi = TRUE)
-  if (!all(colnames(p1) == colnames(p2)))
-    stop("Models appear to have different values on dependent variables.\n")
-  whichCol <- match(m1y, colnames(p1))
-  whichCol2 <- match(m2y, colnames(p2))
-  if (!all(whichCol == whichCol2))
-    stop("Models appear to have different values on dependent variables.\n")
-  m1p <- rep(NA, m1n)
-  m2p <- rep(NA, m2n)
-  for (i in 1:m1n) {
-      m1p[i] <- p1[i, whichCol[i]]
-      m2p[i] <- p2[i, whichCol[i]]
-  }
-  k1 <- length(coef(m1))
-  k2 <- length(coef(m2))
-  lm1p <- log(m1p)
-  lm2p <- log(m2p)
-  m <- lm1p - lm2p
-  bad1 <- is.na(lm1p) | is.nan(lm1p) | is.infinite(lm1p)
-  bad2 <- is.na(lm2p) | is.nan(lm2p) | is.infinite(lm2p)
-  bad3 <- is.na(m) | is.nan(m) | is.infinite(m)
-  bad <- bad1 | bad2 | bad3
-  neff <- sum(!bad)
-  if (any(bad)) {
-    cat("NA or numerical zeros or ones encountered in fitted probabilities\n")
-    cat(paste("dropping these", sum(bad), "cases, but proceed with caution\n"))
-  }
-  aic.factor <- (k1 - k2)/neff
-  bic.factor <- (k1 - k2)/(2 * neff) * log(neff)
-  v <- rep(NA, 3)
-  arg1 <- matrix(m[!bad], nrow = neff, ncol = 3, byrow = FALSE)
-  arg2 <- matrix(c(0, aic.factor, bic.factor), nrow = neff,
-      ncol = 3, byrow = TRUE)
-  num <- arg1 - arg2
-  s <- apply(num, 2, sd)
-  numsum <- apply(num, 2, sum)
-  v <- numsum/(s * sqrt(neff))
-  names(v) <- c("Raw", "AIC-corrected", "BIC-corrected")
-  pval <- rep(NA, 3)
-  msg <- rep("", 3)
-  for (j in 1:3) {
-    if (v[j] > 0) {
-      pval[j] <- 1
-      msg[j] <- "model1 < model2"
+#' importFrom psclpredprob
+vuong_test <- function (m1, m2, digits = getOption("digits"), v = F){
+    m1y <- m1$y
+    m2y <- m2$y
+    m1n <- length(m1y)
+    m2n <- length(m2y)
+    if (m1n == 0 | m2n == 0)
+        stop("Could not extract dependent variables from models.")
+    if (m1n != m2n)
+        stop(paste("Models appear to have different numbers of observations.\n",
+            "Model 1 has ", m1n, " observations.\n", "Model 2 has ",
+            m2n, " observations.\n", sep = ""))
+    if (any(m1y != m2y)) {
+        stop(paste("Models appear to have different values on dependent variables.\n"))
     }
-    else {
-      pval[j] <- pnorm(v[j])
-      msg[j] <- "model1 < model2"
+    p1 <- predprob(m1)
+    p2 <- predprob(m2)
+    if (!all(colnames(p1) == colnames(p2))) {
+        stop("Models appear to have different values on dependent variables.\n")
     }
-  }
-  out <- data.frame(v, msg, format.pval(pval))
-  names(out) <- c("Vuong z-statistic", "H_A", "p-value")
-  return(as.numeric(as.vector(pval[3])))
-}
-
-#' importFrom extraDistr dzinb
-predprob <- function(obj, zi = F){
-  yhat <- predict(obj, type="response")
-  y <- obj$frame$y
-  yUnique <- c(0:max(y))
-  nUnique <- length(yUnique)
-  p <- matrix(NA,length(yhat),nUnique)
-  dimnames(p) <- list(NULL,yUnique)
-  for(i in 1:nUnique){
-    p[, i] <- dnbinom(
-      mu = yhat,
-      size = obj$alpha,
-      x = yUnique[i]
-    )
-    if (zi) {
-      p[yhat == 0, i] <- obj$pz + (1 - obj$pz) * p[yhat == 0, i]
+    whichCol <- match(m1y, colnames(p1))
+    whichCol2 <- match(m2y, colnames(p2))
+    if (!all(whichCol == whichCol2)) {
+        stop("Models appear to have different values on dependent variables.\n")
     }
-  }
-  return(p)
+    m1p <- rep(NA, m1n)
+    m2p <- rep(NA, m2n)
+    for (i in 1:m1n) {
+        m1p[i] <- p1[i, whichCol[i]]
+        m2p[i] <- p2[i, whichCol[i]]
+    }
+    k1 <- length(coef(m1))
+    k2 <- length(coef(m2))
+    lm1p <- log(m1p)
+    lm2p <- log(m2p)
+    m <- lm1p - lm2p
+    bad1 <- is.na(lm1p) | is.nan(lm1p) | is.infinite(lm1p)
+    bad2 <- is.na(lm2p) | is.nan(lm2p) | is.infinite(lm2p)
+    bad3 <- is.na(m) | is.nan(m) | is.infinite(m)
+    bad <- bad1 | bad2 | bad3
+    neff <- sum(!bad)
+    if (any(bad)) {
+        cat("NA or numerical zeros or ones encountered in fitted probabilities\n")
+        cat(paste("dropping these", sum(bad), "cases, but proceed with caution\n"))
+    }
+    aic.factor <- (k1 - k2)/neff
+    bic.factor <- (k1 - k2)/(2 * neff) * log(neff)
+    v <- rep(NA, 3)
+    arg1 <- matrix(m[!bad], nrow = neff, ncol = 3, byrow = FALSE)
+    arg2 <- matrix(c(0, aic.factor, bic.factor), nrow = neff,
+        ncol = 3, byrow = TRUE)
+    num <- arg1 - arg2
+    s <- apply(num, 2, sd)
+    numsum <- apply(num, 2, sum)
+    v <- numsum/(s * sqrt(neff))
+    names(v) <- c("Raw", "AIC-corrected", "BIC-corrected")
+    pval <- rep(NA, 3)
+    msg <- rep("", 3)
+    for (j in 1:3) {
+        if (v[j] > 0) {
+            pval[j] <- 1 - pnorm(v[j])
+            msg[j] <- "model1 > model2"
+        } else {
+            pval[j] <- pnorm(v[j])
+            msg[j] <- "model2 > model1"
+        }
+    }
+    out <- data.frame(v, msg, format.pval(pval))
+    names(out) <- c("Vuong z-statistic", "H_A", "p-value")
+    return(as.numeric(as.vector(pval[3])))
 }
