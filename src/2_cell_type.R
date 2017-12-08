84a53e7ccfd2886444f8caa921847b8c114ba534
Laurent Modolo <laurent.modolo@ens-lyon.fr>
2017-12-08 18:37:15 +0100

    2_cell_type.R: add pls step on DEA results

--- src/2_cell_type.R
+++ src/2_cell_type.R
@@ -244,3 +244,117 @@ done
 /bin/sleep 3600
 done
 ")
+
+################################################################################
+# classification on DEA genes for surface_cell_type
+
+devtools::load_all("../scRNAtools/", reset = T)
+load("results/QC/CB_counts_QC.Rdata")
+
+# build cell_type factor
+phenotype_surface_marker <- scd$getfeature("phenotype_surface_marker")
+phenotype_surface_marker[phenotype_surface_marker == ""] <- NA
+levels(phenotype_surface_marker) <- c("", "MEM", "MEM", "EFF", "EFF", "EFF",
+  "MEM", "Naive", "EFF", "EFF", "EFF", "MEM", "MEM")
+phenotype_surface_marker <- as.factor(as.vector(phenotype_surface_marker))
+scd$setfeature("surface_cell_type", phenotype_surface_marker)
+b_cells <- scd$getfeature("QC_good") %in% T
+
+# load selection off genes and makers to classify on
+genes_PLS <- read.csv("data/genes_PLS.csv")
+surface_marker <- c()
+for (marker_type in colnames(genes_PLS)) {
+  for (marker in genes_PLS[[marker_type]]) {
+    if (marker %in% colnames(scd$getfeatures)) {
+      surface_marker <- c(surface_marker, marker)
+    }
+  }
+}
+load(
+  file = "results/cell_type/mbatch_day_surface_cell_type_DEA.Rdata",
+  v = T
+)
+b_genes <- !is.na(mbatch_day_surface_cell_type_DEA$padj) &
+  mbatch_day_surface_cell_type_DEA$padj < 0.05
+DEA_genes <- mbatch_day_surface_cell_type_DEA$gene[b_genes]
+
+DEA_cell_type_classification <- classification(
+  scd = scd$select(b_cells = b_cells),
+  feature = "surface_cell_type",
+  features = surface_marker,
+  genes = DEA_genes,
+  ncores = 10,
+  algo = "spls_stab",
+  output_file = "results/cell_type/DEA_cell_types"
+)
+save(
+  DEA_cell_type_classification,
+  file = "results/cell_type/DEA_cell_types_all_splsstab.Rdata"
+)
+
+cell_type_groups <- rep(NA, scd$getncells)
+cell_type_groups[b_cells] <- cell_type_classification$groups
+scd$setfeature("DEA_cell_type", cell_type_groups)
+
+save(scd, file = "results/cell_type/CB_counts_QC_DEA_cell_type.Rdata")
+scd_norm <- scd
+load("results/QC/cells_counts_QC.Rdata")
+scd <- scdata$new(
+  infos = scd_norm$getfeatures,
+  counts = scd$getcounts
+)
+save(scd, file = "results/cell_type/cells_counts_QC_DEA_cell_type.Rdata")
+
+
+load("results/cell_type/CB_counts_QC_DEA_cell_type.Rdata")
+ggplot(data = data.frame(
+  ccr7 = scd_norm$select(b_cells = b_cells)$getfeature("ccr7"),
+  il7ra = scd_norm$select(b_cells = b_cells)$getfeature("il7ra"),
+  cell_type = scd_norm$select(b_cells = b_cells)$getfeature("DEA_cell_type")
+), aes(x = ccr7, y = il7ra, color = cell_type)) +
+  geom_point() +
+  theme_bw()
+ggsave(file = "results/cell_type/counts_QC_DEA_cell_type.pdf")
+
+
+system("mkdir -p results/cell_type/pca")
+scRNAtools::pca_plot(
+  scd$select(b_cells = b_cells),
+  color = "DEA_cell_type", color_name = "cell_type",
+  tmp_file = "results/tmp/pca_CB_counts_QC_good_tmp.Rdata",
+  main = "all day"
+)
+ggsave(file = "results/cell_type/pca/pca_counts_QC_DEA_cell_type.pdf")
+for (day in c("D15", "D136", "D593")) {
+  scRNAtools::pca_plot(
+    scd$select(b_cells = b_cells & scd$getfeature("day") %in% day),
+    color = "DEA_cell_type", color_name = "cell_type",
+    tmp_file = paste0("results/tmp/pca_CB_counts_", day, "QC_good_tmp.Rdata"),
+    main = day
+  )
+  ggsave(file = paste0(
+    "results/cell_type/pca/pca_counts_QC_DEA_cell_type_", day, ".pdf"
+  ))
+}
+
+system("mkdir -p results/cell_type/pcmf")
+scRNAtools::pCMF_plot(
+  scd$select(b_cells = b_cells),
+  color = "DEA_cell_type", color_name = "cell_type",
+  tmp_file = "results/tmp/pCMF_CB_counts_QC_good_tmp.Rdata",
+  main = "all day",,
+  ncores = 11
+)
+ggsave(file = "results/cell_type/pcmf/pcmf_counts_QC_DEA_cell_type.pdf")
+for (day in c("D15", "D136", "D593")) {
+  scRNAtools::pCMF_plot(
+    scd$select(b_cells = b_cells & scd$getfeature("day") %in% day),
+    color = "DEA_cell_type", color_name = "cell_type",
+    tmp_file = paste0("results/tmp/pCMF_CB_counts_", day, "QC_good_tmp.Rdata"),
+    main = day,
+    ncores = 11
+  )
+  ggsave(file = paste0(
+    "results/cell_type/pcmf/pcmf_counts_QC_DEA_cell_type_", day, ".pdf"
+  ))
+}
