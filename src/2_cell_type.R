c63e9b2776d8769aae13c5008bb7539b5c57cad6
Laurent Modolo <laurent.modolo@ens-lyon.fr>
2017-12-11 17:53:12 +0100

    2_cell_type.R: add heatmap for first DEA

--- src/2_cell_type.R
+++ src/2_cell_type.R
@@ -33,7 +33,7 @@ phenotype_surface_marker[phenotype_surface_marker == ""] <- NA
 levels(phenotype_surface_marker) <- c("", "MEM", "MEM", "EFF", "EFF", "EFF",
   "MEM", "Naive", "EFF", "EFF", "EFF", "MEM", "MEM")
 phenotype_surface_marker <- as.factor(as.vector(phenotype_surface_marker))
-scd$setfeature("surface_cell_type", phenotype_surface_marker)
+scd$setfeature("phenotype_surface_cell_type", phenotype_surface_marker)
 b_cells <- scd$getfeature("QC_good") %in% T
 
 
@@ -42,7 +42,7 @@ b_cells <- scd$getfeature("QC_good") %in% T
 
 surface_cell_type_classification <- classification(
   scd = scd$select(b_cells = b_cells),
-  feature = "surface_cell_type",
+  feature = "phenotype_surface_cell_type",
   features = surface_marker,
   genes = genes_marker,
   ncores = 10,
@@ -53,9 +53,17 @@ save(
   surface_cell_type_classification,
   file = "results/cell_type/surface_cell_types_all_smplscv.Rdata"
 )
+system("~/scripts/sms.sh \"DEA done\"")
+load("results/cell_type/surface_cell_types_all_smplscv.Rdata")
+
+length(surface_cell_type_classification$groups)
+surface_cell_type_classification$classification$fit_spls$fit$selected
 cell_type_groups <- rep(NA, scd$getncells)
-cell_type_groups[b_cells] <- cell_type_classification$groups
+cell_type_groups[b_cells] <- surface_cell_type_classification$groups
 scd$setfeature("surface_cell_type", cell_type_groups)
+cell_type_pgroups <- rep(NA, scd$getncells)
+cell_type_pgroups[b_cells] <- surface_cell_type_classification$pgroups
+scd$setfeature("psurface_cell_type", cell_type_pgroups)
 
 save(scd, file = "results/cell_type/CB_counts_QC_surface_cell_type.Rdata")
 scd_norm <- scd
@@ -67,12 +75,46 @@ scd <- scdata$new(
 save(scd, file = "results/cell_type/cells_counts_QC_surface_cell_type.Rdata")
 
 load("results/cell_type/CB_counts_QC_surface_cell_type.Rdata")
-ggplot(data = data.frame(
-  ccr7 = scd_norm$select(b_cells = b_cells)$getfeature("ccr7"),
-  il7ra = scd_norm$select(b_cells = b_cells)$getfeature("il7ra"),
-  cell_type = scd_norm$select(b_cells = b_cells)$getfeature("surface_cell_type")
-), aes(x = ccr7, y = il7ra, color = cell_type)) +
+devtools::load_all("../scRNAtools/", reset = T)
+b_cells <- scd$getfeature("QC_good") %in% T
+data_gplot <- data.frame(
+  ccr7 = scd$select(b_cells = b_cells)$getfeature("ccr7"),
+  il7ra = scd$select(b_cells = b_cells)$getfeature("il7ra"),
+  cell_type = scd$select(b_cells = b_cells)$getfeature("phenotype_surface_cell_type")
+)
+cell_type_color <- scRNAtools::cell_type_palette(levels(data_gplot$cell_type))
+data_gplot$cell_type <- as.vector(data_gplot$cell_type)
+data_gplot$cell_type[is.na(data_gplot$cell_type)] <- "unknown"
+data_gplot$cell_type <- as.factor(data_gplot$cell_type)
+data_gplot$ccr7 <- as.numeric(as.vector(data_gplot$ccr7))
+data_gplot$il7ra <- as.numeric(as.vector(data_gplot$il7ra))
+summary(data_gplot)
+cell_type_color <- c(cell_type_color, "gray")
+names(cell_type_color) <- c(names(cell_type_color)[-3], "unknown")
+ggplot() +
+  scale_fill_manual( values = cell_type_color ) +
+  scale_color_manual( values = cell_type_color ) +
+  geom_point(data = data_gplot[data_gplot$cell_type == "unknown", ],
+    aes(x = ccr7, y = il7ra, color = cell_type)) +
+  geom_point(data = data_gplot[data_gplot$cell_type != "unknown", ],
+    aes(x = ccr7, y = il7ra, color = cell_type)) +
+  theme_bw()
+ggsave(file = "results/cell_type/counts_QC_phenotype_surface_cell_type.pdf")
+data_gplot <- data.frame(
+  ccr7 = scd$select(b_cells = b_cells)$getfeature("ccr7"),
+  il7ra = scd$select(b_cells = b_cells)$getfeature("il7ra"),
+  cell_type = scd$select(b_cells = b_cells)$getfeature("surface_cell_type")
+)
+data_gplot$ccr7 <- as.numeric(as.vector(data_gplot$ccr7))
+data_gplot$il7ra <- as.numeric(as.vector(data_gplot$il7ra))
+ggplot(data = data_gplot, aes(x = ccr7, y = il7ra, color = cell_type)) +
   geom_point() +
+  scale_fill_manual(
+      values = scRNAtools::cell_type_palette(levels(data_gplot$cell_type))
+  ) +
+  scale_color_manual(
+    values = scRNAtools::cell_type_palette(levels(data_gplot$cell_type))
+  ) +
   theme_bw()
 ggsave(file = "results/cell_type/counts_QC_surface_cell_type.pdf")
 
@@ -118,8 +160,6 @@ for (day in c("D15", "D136", "D593")) {
     "results/cell_type/pcmf/pcmf_counts_QC_surface_cell_type_", day, ".pdf"
   ))
 }
-
-
 
 ################################################################################
 # DEA on surface_cell_type
@@ -244,6 +284,154 @@ done
 /bin/sleep 3600
 done
 ")
+
+load("results/cell_type/CB_counts_QC_surface_cell_type.Rdata")
+load("results/cell_type/mbatch_day_surface_cell_type_DEA.Rdata")
+b_cells <- scd$getfeature("QC_good") %in% T & !is.na(scd$getfeature("surface_cell_type"))
+table(scd$getgenes %in% expressed(scd$select(b_cells = b_cells)))
+table(is.na(mbatch_day_surface_cell_type_DEA$padj))
+table(mbatch_day_surface_cell_type_DEA$padj < 0.05)
+
+b_genes <- !is.na(mbatch_day_surface_cell_type_DEA$padj) &
+  mbatch_day_surface_cell_type_DEA$padj < 0.05
+DEA_genes <- mbatch_day_surface_cell_type_DEA$gene[b_genes]
+
+system("mkdir -p results/cell_type/pca")
+scRNAtools::pca_plot(
+  scd$select(b_cells = b_cells, genes = DEA_genes),
+  color = "surface_cell_type", color_name = "cell_type",
+  tmp_file = "results/tmp/pca_CB_counts_QC_DEA_surface_cell_type_tmp.Rdata",
+  main = "all day"
+)
+ggsave(file = "results/cell_type/pca/pca_CB_counts_QC_DEA_surface_cell_type.pdf")
+for (day in c("D15", "D136", "D593")) {
+  scRNAtools::pca_plot(
+    scd$select(b_cells = b_cells & scd$getfeature("day") %in% day,
+      genes = DEA_genes),
+    color = "surface_cell_type", color_name = "cell_type",
+    tmp_file = paste0("results/tmp/pca_CB_counts_", day,
+      "QC_DEA_surface_cell_type_tmp.Rdata"),
+    main = day
+  )
+  ggsave(file = paste0(
+    "results/cell_type/pca/pca_CB_counts_QC_DEA_surface_cell_type_", day, ".pdf"
+  ))
+}
+
+system("mkdir -p results/cell_type/pcmf")
+scRNAtools::pCMF_plot(
+  scd$select(b_cells = b_cells, genes = DEA_genes),
+  color = "surface_cell_type", color_name = "cell_type",
+  tmp_file = "results/tmp/pCMF_CB_counts_QC_DEA_surface_cell_type_tmp.Rdata",
+  main = "all day",,
+  ncores = 11
+)
+ggsave(
+  file = "results/cell_type/pcmf/pcmf_CB_counts_QC_DEA_surface_cell_type.pdf"
+)
+for (day in c("D15", "D136", "D593")) {
+  scRNAtools::pCMF_plot(
+    scd$select(b_cells = b_cells & scd$getfeature("day") %in% day,
+      genes = DEA_genes),
+    color = "surface_cell_type", color_name = "cell_type",
+    tmp_file = paste0("results/tmp/pCMF_CB_counts_", day,
+      "QC_DEA_surface_cell_type_tmp.Rdata"),
+    main = day,
+    ncores = 11
+  )
+  ggsave(file = paste0(
+    "results/cell_type/pcmf/pcmf_CB_counts_QC_DEA_surface_cell_type_", day, ".pdf"
+  ))
+}
+
+
+load("results/cell_type/CB_counts_QC_surface_cell_type.Rdata")
+load("results/cell_type/mbatch_day_surface_cell_type_DEA.Rdata")
+b_cells <- scd$getfeature("QC_good") %in% T & !is.na(scd$getfeature("surface_cell_type"))
+b_genes <- !is.na(mbatch_day_surface_cell_type_DEA$padj) &
+  mbatch_day_surface_cell_type_DEA$padj < 0.05
+DEA_genes <- mbatch_day_surface_cell_type_DEA$gene[b_genes]
+
+system("mkdir -p results/cell_type/heatmap/")
+surface_cell_type_palette <- cell_type_palette
+devtools::load_all("../scRNAtools/", reset = T)
+hm <- heatmap_genes(
+  scd = scd$select(b_cells = b_cells, genes = DEA_genes),
+  features = c("surface_cell_type", "day", "psurface_cell_type"),
+  cells_order = order(
+    scd$select(b_cells = b_cells)$getfeature("day"),
+    as.numeric(as.vector(
+      scd$select(b_cells = b_cells)$getfeature("psurface_cell_type")
+    ))
+  ),
+  genes_order = order(scd$select(b_cells = b_cells, genes = DEA_genes)$getgenes),
+  title = "DE genes between surface_cell_type",
+  factor = c(T, T, F),
+  file = "results/cell_type/heatmap/hm_CB_counts_QC_DEA_surface_cell_type.pdf"
+)
+print(hm)
+hm_corr <- heatmap_corr_genes(
+  scd = scd$select(b_cells = b_cells, genes = DEA_genes),
+  features = c("surface_cell_type", "day", "psurface_cell_type"),
+  cells_order = order(
+    scd$select(b_cells = b_cells)$getfeature("day"),
+    as.numeric(as.vector(
+      scd$select(b_cells = b_cells)$getfeature("psurface_cell_type")
+    ))
+  ),
+  title = "corr DE genes between surface_cell_type",
+  factor = c(T, T, F),
+  file = "results/cell_type/heatmap/hm_corr_CB_counts_QC_DEA_surface_cell_type.pdf"
+)
+print(hm_corr)
+
+for (day in c("D15", "D136", "D593")) {
+  hm <- heatmap_genes(
+    scd = scd$select(
+      b_cells = b_cells & scd$getfeature("day") %in% day,
+      genes = DEA_genes
+    ),
+    features = c("surface_cell_type", "day", "psurface_cell_type"),
+    cells_order = order(
+      as.numeric(as.vector(
+        scd$select(b_cells = b_cells & scd$getfeature("day") %in% day)$
+          getfeature("psurface_cell_type")
+      ))
+    ),
+    genes_order = order(
+      scd$select(b_cells = b_cells & scd$getfeature("day") %in% day,
+        genes = DEA_genes)$getgenes
+    ),
+    title = paste0("DE genes between surface_cell_type ", day),
+    factor = c(T, T, F),
+    file = paste0(
+      "results/cell_type/heatmap/hm_CB_counts_QC_DEA_surface_cell_type_",
+      day, ".pdf"
+    )
+  )
+  print(hm)
+  hm_corr <- heatmap_corr_genes(
+    scd = scd$select(
+      b_cells = b_cells & scd$getfeature("day") %in% day,
+      genes = DEA_genes
+    ),
+    features = c("surface_cell_type", "day", "psurface_cell_type"),
+    cells_order = order(
+      as.numeric(as.vector(
+        scd$select(b_cells = b_cells & scd$getfeature("day") %in% day)$
+          getfeature("psurface_cell_type")
+      ))
+    ),
+    title = paste0("DE genes between surface_cell_type ", day),
+    factor = c(T, T, F),
+    file = paste0(
+      "results/cell_type/heatmap/hm_corr_CB_counts_QC_DEA_surface_cell_type_",
+      day, ".pdf"
+    )
+  )
+  print(hm_corr)
+}
+
 
 ################################################################################
 # classification on DEA genes for surface_cell_type
