04e10bc71de200d0735c0366fe04c4aa4dabcd5e
Laurent Modolo <laurent.modolo@ens-lyon.fr>
2017-12-12 19:07:39 +0100

    classification.R: add possibility to have train and predit set of same size

--- R/classification.R
+++ R/classification.R
@@ -21,7 +21,8 @@ classification <- function(
   genes,
   algo = "spls_pls",
   selection = "stab",
-  output_file = "") {
+  output_file = "",
+  v = F) {
 
   to_train_on <- !is.na(scd$getfeature(feature))
   print("training on:")
@@ -29,13 +30,18 @@ classification <- function(
     scd$select(b_cells = to_train_on)$getfeature(feature)
   )
   model_type <- scRNAtools::pls_type(as.factor(group_by$names))
-  print("building training set...")
-  data_train <- scRNAtools::get_data(scd, to_train_on, features, genes)
 
-  rm_data_train <- clean_data(data_train, group_by)
+  print("building training set...")
+  rm_data_train <- scRNAtools::get_data(
+    scd = scd,
+    b_cells = to_train_on,
+    features = features,
+    genes = genes,
+    group_by = group_by,
+    v = v
+  )
   data_train <- rm_data_train$data
   group_by <- rm_data_train$group_by
-
   print("training PLS...")
   algo_training <- get(paste0(model_type, "_", algo , "_training"))
   training <- algo_training(
@@ -46,8 +52,14 @@ classification <- function(
   )
 
   print("building set to predict...")
-  data <- scRNAtools::get_data(scd, !to_train_on, features, genes)
-  rm_data <- clean_data(data, group_by)
+  rm_data <- scRNAtools::get_data(
+    scd = scd,
+    b_cell = !to_train_on,
+    features = features,
+    genes = genes,
+    group_by = group_by,
+    v = v
+  )
   data <- rm_data$data
   print("predicting from PLS model...")
   algo_classification <- get(paste0(model_type, "_", algo , "_classification"))
@@ -86,37 +98,59 @@ logspace <- function( d1, d2, n) {
   return(exp(log(10) * seq(d1, d2, length.out = n)))
 }
 
-get_data <- function(scd, b_cells, features, genes) {
+get_data <- function(scd, b_cells, features, genes, group_by, v = TRUE) {
   if (missing(features) & missing(genes)) {
     stop("error: missing features and / or genes to classify on")
   }
+  data <- scRNAtools::get_features(
+    scd = scd, features = features
+  )
+  data <- cbind(
+    data,
+    scRNAtools::get_genes(scd = scd, genes = genes)
+  )
+  data <- apply(as.matrix(data), c(1, 2), as.numeric)
+  data <- apply(data, 2, scRNAtools::ascb)
+  row_rm <- scRNAtools::clean_data(data = data, group_by = group_by)
+  if (!any(b_cells[!row_rm])) {
+    if (v) {
+      print("warning: predicted set empty. Using predicting set.")
+    }
+    b_cells <- rep(TRUE, nrow(data))
+  }
+  print(dim(data[b_cells & !row_rm, ]))
+  return(
+    list(
+      data = data[b_cells & !row_rm, ],
+      group_by = group_by[b_cells & !row_rm],
+      row_rm = row_rm[b_cells]
+    )
+  )
+}
+
+get_features <- function(scd, features, v = TRUE) {
   data <- c()
   if (!missing(features)) {
     if (length(features) == 1) {
-      data <- scd$select(
-        b_cells = b_cells)$getfeature(features)
+      data <- scd$getfeature(features)
     } else {
-      data <- scd$select(b_cells = b_cells, features = features)$getfeatures
+      data <- scd$select(features = features)$getfeatures
     }
   }
+  return(data)
+}
+
+get_genes <- function(scd, genes, v = TRUE) {
+  data <- c()
   if (!missing(genes)) {
     if (length(genes) == 1) {
-      data <- cbind(
-        data,
-        scd$select(b_cells = b_cells)$getgene(genes)
-      )
+      data <- scd$getgene(genes)
     } else {
-      data <- cbind(
-        data,
-        scd$select(b_cells = b_cells)$getcounts[,
+      data <- scd$getcounts[,
           which(scd$getgenes %in% genes)
         ]
-      )
     }
   }
-  data <- apply(as.matrix(data), c(1, 2), as.numeric)
-  data <- apply(data, 2, scRNAtools::ascb)
-  print(dim(data))
   return(data)
 }
 
@@ -128,16 +162,7 @@ clean_data <- function(data, group_by) {
       any(is.na(x))
     }
   )
-  if (missing(group_by)) {
-    return(list(
-      data = data[!row_rm, ], row_rm = row_rm
-    ))
-  } else {
-    group_by$by <- group_by$by[!row_rm]
-    return(list(
-      data = data[!row_rm, ], group_by = group_by, row_rm = row_rm
-    ))
-  }
+  return(row_rm)
 }
 
 group_by_transform <- function(by) {
@@ -361,7 +386,7 @@ multinomial_pls_cv_classification <- function(
 
 #' @importFrom plsgenomics logistic.spls.stab
 logistic_spls_stab_training <- function(by, data, ncores, file) {
-  if (!missing(file) & file.exists(paste0(file, "_msplsstab.Rdata"))) {
+  if (!missing(file) & file.exists(paste0(file, "_lsplsstab.Rdata"))) {
     print(paste0(file, "_lsplsstab.Rdata found. skipping training step..."))
     load(paste0(file, "_lsplsstab.Rdata"))
   } else {
@@ -508,7 +533,7 @@ logistic_pls_stab_training <- function(by, data, ncores, file) {
 
 #' @importFrom plsgenomics mrpls.cv
 logistic_pls_cv_training <- function(by, data, ncores, file){
-  if (!missing(file) & file.exists(paste0(file, "lplscv.Rdata"))) {
+  if (!missing(file) & file.exists(paste0(file, "_lplscv.Rdata"))) {
     print(paste0(file, "_lplscv.Rdata found. skipping training step..."))
     load(paste0(file, "_lplscv.Rdata"))
   } else {
@@ -541,6 +566,8 @@ logistic_pls_cv_classification <- function(
     print(paste0(file, "_lpls.Rdata found. skipping classification step..."))
     load(paste0(file, "_lpls.Rdata"))
   } else {
+    print(dim(data))
+    print(dim(data_train))
     model <- plsgenomics::rpls(
       Xtrain = data_train,
       Ytrain = fit$group_by$by,
@@ -556,407 +583,3 @@ logistic_pls_cv_classification <- function(
   }
   return(list(fit = fit, model = model))
 }
-
-
-#
-# #' @import plsgenomics
-# spls_training <- function(
-#   by, data, ncores = 1, file, force, skip, randomize) {
-#   data <- as.matrix(data)
-#   is_group <- factorize(by)
-#   groups_names <- levels(as.factor(by))
-#   is_group <- as.numeric(is_group) - 1
-#   print("sparse pls tuning")
-#   print(table(is_group))
-#   model_tunning <- logit.spls.cv(
-#     X = data,
-#     Y = is_group,
-#     lambda.ridge.range = signif(logspace(d1 = -2, d2 = 3, n = 21), digits = 3),
-#     lambda.l1.range = seq(0.05,0.95,by = 0.1),
-#     ncomp.range = c(1:min(4,ncol(data) - 1)),
-#     adapt = FALSE,
-#     maxIter = 100,
-#     svd.decompose = FALSE,
-#     return.grid = TRUE,
-#     ncores = ncores,
-#     nfolds = 5,
-#     center.X = TRUE,
-#     scale.X = TRUE,
-#     weighted.center = TRUE,
-#     nrun = 50,
-#     verbose = TRUE)
-#   parameters <- list(
-#     is_group = is_group,
-#     lambda.ridge = model_tunning$lambda.ridge.opt,
-#     lambda.l1 = model_tunning$lambda.l1.opt,
-#     ncomp = model_tunning$ncomp.opt,
-#     groups_names = groups_names
-#   )
-#   model <- list(
-#     model = NA,
-#     parameters = parameters,
-#     cv_error = model_tunning$cv.grid,
-#     fit = model_tunning
-#   )
-#   if (!missing(file)) {
-#     save(model, file = paste0(file, ".Rdata"))
-#   }
-#   return(model)
-# }
-#
-# #' @import plsgenomics
-# stab_spls_training <- function(
-#   by, data, ncores = 1, file, force, skip, randomize) {
-#   data <- as.matrix(data)
-#   is_group <- factorize(by)
-#   groups_names <- levels(as.factor(by))
-#   is_group <- as.numeric(is_group) - 1
-#   print("sparse pls tuning")
-#   print(table(is_group))
-#   model_tunning <- logit.spls.stab(
-#     X = data,
-#     Y = is_group,
-#     lambda.ridge.range = signif(
-#       logspace(d1 = -2, d2 = 3, n = 21), digits = 3
-#     ),
-#     lambda.l1.range = seq(0.05,0.95,by = 0.05),
-#     ncomp = 1,
-#     adapt = FALSE,
-#     maxIter = 100,
-#     svd.decompose = FALSE,
-#     ncores = ncores,
-#     nfolds = 5,
-#     center.X = TRUE,
-#     scale.X = TRUE,
-#     weighted.center = TRUE,
-#     nresamp = 100,
-#     verbose = TRUE
-#   )
-#   if (!missing(file)) {
-#     save(model_tunning, file = paste0(file, ".Rdata"))
-#   }
-#   return(model_tunning)
-# }
-#
-# #' @import plsgenomics
-# spls_classification <- function(
-#   fit, data, data_train, file) {
-#   data <- as.matrix(data)
-#   data_train <- as.matrix(data_train)
-#   print("sparse pls classification")
-#   print("min error")
-#   print(min(fit$cv_error$error))
-#   model <- logit.spls(
-#     Xtrain = data_train,
-#     Ytrain = fit$parameters$is_group,
-#     lambda.ridge = fit$parameters$lambda.ridge,
-#     lambda.l1 = fit$parameters$lambda.l1,
-#     ncomp = fit$parameters$ncomp,
-#     Xtest = data,
-#     adapt = TRUE,
-#     maxIter = 100,
-#     svd.decompose = FALSE,
-#     center.X = TRUE,
-#     scale.X = TRUE,
-#     weighted.center = TRUE
-#   )
-#   model$groups_names <- fit$parameters$groups_names
-#   model$groups <- model$groups_names[model$hatYtest + 1]
-#   if (!missing(file)) {
-#     save(model, file = paste0(file, ".Rdata"))
-#   }
-#   return(model)
-# }
-#
-# ################################################################################
-# # sparse logistic pls
-#
-# #' @import plsgenomics
-# mspls_training <- function(
-#   by, data, ncores = 1, file, force, skip, randomize) {
-#   data <- as.matrix(data)
-#   is_group <- factorize(by)
-#   groups_names <- levels(as.factor(by))
-#   is_group <- as.numeric(is_group) - 1
-#   print("sparse multinomial pls tuning")
-#   print(table(is_group))
-#   model_tunning <- multinom.spls.cv(
-#     X = data,
-#     Y = is_group,
-#     lambda.ridge.range = signif(
-#       logspace(d1 = -2, d2 = 3, n = 21), digits = 3
-#     ),
-#     lambda.l1.range = seq(0.05,0.95,by = 0.1),
-#     ncomp.range = c(1:min(4,ncol(data) - 1)),
-#     adapt = FALSE,
-#     maxIter = 100,
-#     svd.decompose = FALSE,
-#     return.grid = TRUE,
-#     ncores = ncores,
-#     nfolds = 5,
-#     center.X = TRUE,
-#     scale.X = TRUE,
-#     weighted.center = TRUE,
-#     nrun = 50,
-#     verbose = TRUE)
-#   parameters <- list(
-#     is_group = is_group,
-#     lambda.ridge = model_tunning$lambda.ridge.opt,
-#     lambda.l1 = model_tunning$lambda.l1.opt,
-#     ncomp = model_tunning$ncomp.opt,
-#     groups_names = groups_names
-#   )
-#   model <- list(
-#     model = NA,
-#     parameters = parameters,
-#     cv_error = model_tunning$cv.grid,
-#     fit = model_tunning
-#   )
-#   if (!missing(file)) {
-#     save(model, file = paste0(file, ".Rdata"))
-#   }
-#   return(model)
-# }
-#
-# #' @import plsgenomics
-# stab_mspls_training <- function(by, data, ncores = 1, file, force, skip,
-#     randomize) {
-#   data <- as.matrix(data)
-#   is_group <- factorize(by)
-#   is_group <- as.numeric(is_group) - 1
-#   print("sparse multinomial pls tuning")
-#   print(table(is_group))
-#   model_tunning <- plsgenomics::multinom.spls.stab(
-#     X = data,
-#     Y = is_group,
-#     lambda.ridge.range = signif(
-#       logspace(d1 = -2, d2 = 3, n = 21), digits = 3
-#     ),
-#     lambda.l1.range = seq(0.05, 0.95, by = 0.05),
-#     ncomp.range = c(1:3),
-#     svd.decompose = FALSE,
-#     ncores = ncores,
-#     center.X = TRUE,
-#     scale.X = TRUE,
-#     weighted.center = TRUE,
-#     verbose = TRUE
-#   )
-#   if (!missing(file)) {
-#     save(model_tunning, file = paste0(file, ".Rdata"))
-#   }
-#   return(model_tunning)
-# }
-#
-# #' @import plsgenomics
-# mspls_classification <- function(fit, data, data_train, file) {
-#   data <- as.matrix(data)
-#   data_train <- as.matrix(data_train)
-#   print("sparse multinomial pls classification")
-#   print("min error")
-#   print(min(fit$cv_error$error))
-#   model <- multinom.spls(
-#     Xtrain = data_train,
-#     Ytrain = fit$parameters$is_group,
-#     lambda.ridge = fit$parameters$lambda.ridge,
-#     lambda.l1 = fit$parameters$lambda.l1,
-#     ncomp = fit$parameters$ncomp,
-#     Xtest = data,
-#     adapt = TRUE,
-#     maxIter = 100,
-#     svd.decompose = FALSE,
-#     center.X = TRUE,
-#     scale.X = TRUE,
-#     weighted.center = TRUE
-#   )
-#   model$groups_names <- fit$parameters$groups_names
-#   model$groups <- model$groups_names[model$hatYtest + 1]
-#   if (!missing(file)) {
-#     save(model, file = paste0(file, ".Rdata"))
-#   }
-#   return(model)
-# }
-#
-# ################################################################################
-# # sparse binomial pls + binomial pls
-#
-# #' @import plsgenomics
-# spls_pls_training <- function(
-#   by, data, ncores = 1, file, force, skip = 0, randomize = FALSE) {
-#   if (randomize) {
-#     classification_sparse_r <- vector("list", 100)
-#     classification_sparse_r <- lapply(
-#       classification_sparse_r,
-#       FUN = function(x, by, data, file, ncores) {
-#           r_select <- sample(
-#             rownames(data),
-#             round(nrow(data) * 0.20)
-#           )
-#           training_sparse <- stab_spls_training(
-#             factorize(by[r_select]),
-#             data[r_select,],
-#             ncores,
-#             paste0(file, "spls_pls_training_a_r"))
-#           classification_sparse <- spls_classification(
-#             training_sparse,
-#             data,
-#             data[r_select,],
-#             paste0(file, "spls_pls_training_b_r")
-#           )
-#           retun(
-#             list(
-#               training_sparse = training_sparse,
-#               classification_sparse = classification_sparse,
-#               r_select = r_select
-#             )
-#           )
-#         },
-#         by,
-#         data,
-#         file,
-#         ncores
-#       )
-#     return(classification_sparse_r)
-#   }
-#   if (skip <= 0) {
-#     training_sparse <- stab_spls_training(
-#       by = by,
-#       data = data,
-#       ncores = ncores,
-#       file = paste0(file, "spls_pls_training_a")
-#     )
-#     skip <- 1
-#   }
-#   if (skip <= 1) {
-#     if (!exists("training_sparse")) {
-#       load(paste0(file, "spls_pls_training_a.Rdata"))
-#       training_sparse <- model
-#     }
-#     classification_sparse <- spls_classification(
-#       fit = training_sparse,
-#       data = data,
-#       data_train = data,
-#       file = paste0(file, "spls_pls_training_b")
-#     )
-#     skip <- 2
-#   }
-#   if (!exists("classification_sparse")) {
-#     load(paste0(file, "spls_pls_training_b.Rdata"))
-#     classification_sparse <- model
-#   }
-#   c_select <- classification_sparse$A
-#   print("selected:")
-#   print(colnames(data)[c_select])
-#   if (!missing(force)) {
-#     print("forced:")
-#     print(force)
-#     if (length(force) >= 1) {
-#       if (!is.numeric(force[1])) {
-#         force <- which(colnames(data) %in% force)
-#       }
-#     }
-#     c_select <- unique(c(c_select, force))
-#     print("selected + forced:")
-#     print(colnames(data)[c_select])
-#   }
-#   if (skip <= 2) {
-#     training <- pls_training(
-#       by = by,
-#       data = data[ ,c_select],
-#       ncores = ncores,
-#       file = paste0(file, "spls_pls_training_c")
-#     )
-#   }else {
-#     if (!exists("training")) {
-#       load(paste0(file, "mspls_mpls_training_c.Rdata"))
-#       training <- model
-#     }
-#   }
-#   training$A.full <- c_select
-#   training$training_sparse <- training_sparse
-#   training$classification_sparse <- classification_sparse
-#   save(training, training_sparse, classification_sparse,
-#     file = paste0(file, "mspls_mpls_training_all"))
-#   return(training)
-# }
-#
-# spls_pls_classification <- function(fit, data, data_train, file) {
-#   return(pls_classification(
-#     fit = fit,
-#     data = data[ ,fit$A.full],
-#     data_train = data_train[ ,fit$A.full],
-#     file = file)
-#   )
-# }
-#
-# ################################################################################
-# # sparse multinomial pls + multinomial pls
-#
-# mspls_mpls_training <- function(by, data, ncores = 1, file, force = c(), skip = 0, randomize = FALSE) {
-#   if (skip <= 0) {
-#     training_sparse <- stab_mspls_training(
-#       by = by,
-#       data = data,
-#       ncores = ncores,
-#       file = paste0(file, "mspls_mpls_training_a")
-#     )
-#     skip <- 1
-#   }
-#   if (skip <= 1) {
-#     if (!exists("training_sparse")) {
-#       load(paste0(file, "mspls_mpls_training_a.Rdata"))
-#       training_sparse <- model
-#     }
-#     classification_sparse <- mspls_classification(
-#       fit = training_sparse,
-#       data = data,
-#       data_train = data,
-#       file = paste0(file, "mspls_mpls_training_b")
-#     )
-#     skip <- 2
-#   }
-#   if (!exists("classification_sparse")) {
-#     load(paste0(file, "mspls_mpls_training_b.Rdata"))
-#     classification_sparse <- model
-#   }
-#   c_select <- classification_sparse$A.full
-#   print("selected:")
-#   print(colnames(data)[c_select])
-#   if (!missing(force)) {
-#     print("forced:")
-#     print(force)
-#     if (length(force) >= 1) {
-#       if (!is.numeric(force[1])) {
-#         force <- which(colnames(data) %in% force)
-#       }
-#     }
-#     c_select <- unique(c(c_select, force))
-#     print("selected + forced:")
-#     print(colnames(data)[c_select])
-#   }
-#   if (skip <= 2) {
-#     training <- mpls_training(
-#       by, data[,c_select], ncores, paste0(file, "mspls_mpls_training_c")
-#     )
-#     skip <- 3
-#   }else {
-#     if (!exists("training")) {
-#       load(paste0(file, "mspls_mpls_training_c.Rdata"))
-#       training <- model
-#     }
-#   }
-#   training$A.full <- c_select
-#   training$training_sparse <- training_sparse
-#   training$classification_sparse <- classification_sparse
-#   save(training, training_sparse, classification_sparse,
-#     file = paste0(file, "mspls_mpls_training_all"))
-#   return(training)
-# }
-#
-# mspls_mpls_classification <- function(fit, data, data_train, file) {
-#   return(mpls_classification(
-#     fit = fit,
-#     data = data[ ,fit$A.full],
-#     data_train = data_train[ ,fit$A.full],
-#     file = file)
-#   )
-# }
