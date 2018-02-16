48d8922600cf5acde3e6048fc39f3b464dc10815
Laurent Modolo <laurent.modolo@ens-lyon.fr>
2018-02-16 17:31:06 +0100

    1_QC.R: add quality control for F donor

--- src/1_QC.R
+++ src/1_QC.R
@@ -451,3 +451,75 @@ for (day in c("D15", "D136", "D593")) {
   )
   print(batch_estimate_qc_norm[[day]]$summary)
 }
+
+################################################################################
+# QC of F data
+
+setwd("~/projects/yellow_fever/")
+library(scRNAtools)
+devtools::load_all("../scRNAtools/", reset = T)
+load("results/QC/counts_QC.Rdata")
+
+scd$setfeature("QC_score",
+  ifelse(scd$getfeature("sex") %in% "F",
+    0.5,
+    scd$getfeature("QC_score")
+  )
+)
+scd$setfeature("to_QC",
+  ifelse(scd$getfeature("sex") %in% "F",
+    TRUE,
+    scd$getfeature("to_QC")
+  )
+)
+scRNAtools::QC_classification(
+  scd = scd,
+  is_blank = scd$getfeature("QC_score") <= 0.5
+)
+save(scd, file = "results/QC/counts_QC_F.Rdata")
+
+b_cells = scd$getfeature('sex') %in% "F"
+summary(scd$select(b_cells = b_cells)$getfeature("QC_good"))
+
+scRNAtools::pca_plot(
+  scd$select(b_cells = b_cells), color = "QC_good", color_name = "antigen",
+  tmp_file = "results/tmp/pca_counts_F_tmp.Rdata",
+  main = "all day"
+)
+
+b_cells = scd$getfeature('sex') %in% "F" & scd$getfeature("QC_good") %in% T
+scRNAtools::pca_plot(
+  scd$select(b_cells = b_cells), color = "batch", color_name = "clonality",
+  tmp_file = "results/tmp/pca_counts_QC_F_tmp.Rdata",
+  main = "all day"
+)
+
+scRNAtools::pca_plot(
+  scd$select(b_cells = b_cells), color = "day", color_name = "day",
+  tmp_file = "results/tmp/pca_counts_QC_F_tmp.Rdata",
+  main = "all day"
+)
+
+# cells effect normalization
+load("results/QC/counts_QC_F.Rdata")
+b_cells = scd$getfeature('sex') %in% "F" & scd$getfeature("QC_good") %in% T
+scd <- normalize(
+  scd = scd,
+  b_cells = b_cells,
+  method = "SCnorm",
+  cpus = 4,
+  tmp_file = "results/tmp/normalization_F_tmp.Rdata"
+)
+save(scd, file = "results/QC/cells_counts_QC_F.Rdata")
+
+load("results/QC/cells_counts_QC_F.Rdata")
+for (day in c("D15", "D90")) {
+    scd <- normalize(
+    scd = scd,
+    b_cells = b_cells & scd$getfeature("day") %in% day,
+    method = "ComBat",
+    cpus = 5,
+    tmp_file = paste0("results/tmp/normalization_cells_combat_,", day, "_F_tmp.Rdata")
+  )
+}
+save(scd, file = "results/QC/CB_counts_QC_F.Rdata")
