6fd03e6aba63798c95393221e5242e77e23682ec
Laurent Modolo <laurent.modolo@ens-lyon.fr>
2018-02-26 17:42:32 +0100

    2_4_cell_type_DEA_DEA.R: add PCA and pCMF plots

--- src/2_4_cell_type_DEA_DEA.R
+++ src/2_4_cell_type_DEA_DEA.R
@@ -34,11 +34,13 @@ b_cells <- scd$getfeature("QC_good") %in% T &
   !is.na(scd$getfeature("DEA_cell_type")) &
   scd$getfeature("sex") %in% "M"
 DEA_cell_type_palette <- cell_type_palette
+DEA_genes_M <- c()
 for (day in c("D15", "D136", "D593")) {
   load(paste0("results/cell_type/mbatch_", day, "_DEA_cell_type_DEA.Rdata"))
   b_genes <- !is.na(mbatch_DEA_cell_type_DEA$padj) &
     mbatch_DEA_cell_type_DEA$padj < 0.05
   DEA_genes <- mbatch_DEA_cell_type_DEA$gene[b_genes]
+  DEA_genes_M <- unique(c(DEA_genes, DEA_genes_M))
   scd_norm <- zinorm(
     scd = scd$select(
       b_cells = b_cells & scd$getfeature("day") %in% day,
@@ -92,6 +94,46 @@ for (day in c("D15", "D136", "D593")) {
   print(hm_corr)
 }
 
+system("mkdir -p results/cell_type/pca")
+system("mkdir -p results/cell_type/pcmf")
+b_cells <- scd$getfeature("QC_good") %in% T &
+  !is.na(scd$getfeature("DEA_cell_type")) &
+  scd$getfeature("sex") %in% "M"
+DEA_cell_type_palette <- cell_type_palette
+for (day in c("D15", "D136", "D593")) {
+  load(paste0("results/cell_type/mbatch_", day, "_DEA_cell_type_DEA.Rdata"))
+  b_genes <- !is.na(mbatch_DEA_cell_type_DEA$padj) &
+    mbatch_DEA_cell_type_DEA$padj < 0.05
+  DEA_genes <- mbatch_DEA_cell_type_DEA$gene[b_genes]
+  scd_norm <- zinorm(
+    scd = scd$select(
+      b_cells = b_cells & scd$getfeature("day") %in% day,
+      genes = DEA_genes
+    ),
+    cpus = 10,
+    file = paste0("results/tmp/zi_norm_cells_counts_QC_DEA_cell_type_",
+      day, ".RData")
+  )
+  scRNAtools::pca_plot(
+    scd_norm,
+    color = "DEA_cell_type", color_name = "cell_type",
+    tmp_file = paste0("results/tmp/pca_zi_norm_counts_QC_DEA_cell_type_", day, ".Rdata"),
+    main = day
+  )
+  ggsave(file = paste0(
+    "results/cell_type/pca/pca_zi_norm_counts_QC_DEA_cell_type_pca", day, ".pdf"
+  ))
+  scRNAtools::pCMF_plot(
+    scd$select(b_cells = b_cells & scd$getfeature("day") %in% day),
+    color = "DEA_cell_type", color_name = "cell_type",
+    tmp_file = paste0("results/tmp/pCMF_CB_counts_QC_DEA_cell_type_", day, ".Rdata"),
+    main = day,
+    ncores = 11
+  )
+  ggsave(file = paste0(
+    "results/cell_type/pcmf/pCMF_CB_counts_QC_DEA_cell_type_", day, ".pdf"
+  ))
+}
 ################################################################################
 ## DEA for the F donor
 
@@ -105,7 +147,7 @@ for (day in c("D15", "D90")) {
     scd$getfeature("sex") %in% "F" &
     !is.na(scd$getfeature("DEA_cell_type")) &
     scd$getfeature("day") %in% day
-  mbatch_surface_cell_type_DEA <- DEA(
+  mbatch_DEA_cell_type_DEA <- DEA(
     scd = scd,
     formula_null = "y ~ (1|batch)",
     formula_full = "y ~ (1|batch) + DEA_cell_type",
@@ -115,25 +157,28 @@ for (day in c("D15", "D90")) {
     folder_name = paste0("results/cell_type/mbatch_", day, "DEA_cell_type_DEA_F")
   )
   save(
-    mbatch_surface_cell_type_DEA,
+    mbatch_DEA_cell_type_DEA,
     file = paste0("results/cell_type/mbatch_", day, "_DEA_cell_type_DEA_F.Rdata")
   )
   system("~/scripts/sms.sh \"DEA done\"")
   print(day)
-  print(table(is.na(mbatch_surface_cell_type_DEA$padj)))
-  print(table(mbatch_surface_cell_type_DEA$padj < 0.05))
+  print(table(is.na(mbatch_DEA_cell_type_DEA$padj)))
+  print(table(mbatch_DEA_cell_type_DEA$padj < 0.05))
 }
+
 
 system("mkdir -p results/cell_type/heatmap/")
 b_cells <- scd$getfeature("QC_good") %in% T &
   !is.na(scd$getfeature("DEA_cell_type")) &
   scd$getfeature("sex") %in% "F"
 DEA_cell_type_palette <- cell_type_palette
+DEA_genes_F <- c()
 for (day in c("D15", "D90")) {
   load(paste0("results/cell_type/mbatch_", day, "_DEA_cell_type_DEA_F.Rdata"))
   b_genes <- !is.na(mbatch_DEA_cell_type_DEA$padj) &
     mbatch_DEA_cell_type_DEA$padj < 0.05
   DEA_genes <- mbatch_DEA_cell_type_DEA$gene[b_genes]
+  DEA_genes_F <- unique(c(DEA_genes, DEA_genes_F))
   scd_norm <- zinorm(
     scd = scd$select(
       b_cells = b_cells & scd$getfeature("day") %in% day,
@@ -186,3 +231,56 @@ for (day in c("D15", "D90")) {
   )
   print(hm_corr)
 }
+
+system("mkdir -p results/cell_type/pca")
+system("mkdir -p results/cell_type/pcmf")
+b_cells <- scd$getfeature("QC_good") %in% T &
+  !is.na(scd$getfeature("DEA_cell_type")) &
+  scd$getfeature("sex") %in% "F"
+DEA_cell_type_palette <- cell_type_palette
+DEA_genes_F <- c()
+for (day in c("D15", "D90")) {
+  load(paste0("results/cell_type/mbatch_", day, "_DEA_cell_type_DEA_F.Rdata"))
+  b_genes <- !is.na(mbatch_DEA_cell_type_DEA$padj) &
+    mbatch_DEA_cell_type_DEA$padj < 0.05
+  DEA_genes <- mbatch_DEA_cell_type_DEA$gene[b_genes]
+  scd_norm <- zinorm(
+    scd = scd$select(
+      b_cells = b_cells & scd$getfeature("day") %in% day,
+      genes = DEA_genes
+    ),
+    cpus = 10,
+    file = paste0("results/tmp/zi_norm_cells_counts_QC_DEA_cell_type_",
+      day, "_F.RData")
+  )
+  scRNAtools::pca_plot(
+    scd_norm,
+    color = "DEA_cell_type", color_name = "cell_type",
+    tmp_file = paste0("results/tmp/pca_zi_norm_counts_QC_DEA_cell_type_", day, "_F.Rdata"),
+    main = day
+  )
+  ggsave(file = paste0(
+    "results/cell_type/pca/pca_zi_norm_counts_QC_DEA_cell_type_pca", day, "_F.pdf"
+  ))
+  scRNAtools::pCMF_plot(
+    scd$select(b_cells = b_cells & scd$getfeature("day") %in% day),
+    color = "DEA_cell_type", color_name = "cell_type",
+    tmp_file = paste0("results/tmp/pCMF_CB_counts_QC_DEA_cell_type_", day, "_F.Rdata"),
+    main = day,
+    ncores = 11
+  )
+  ggsave(file = paste0(
+    "results/cell_type/pcmf/pCMF_CB_counts_QC_DEA_cell_type_", day, "_F.pdf"
+  ))
+}
+################################################################################
+## DEA for the F donor
+
+################################################################################
+
+length(DEA_genes_M)
+
+length(DEA_genes_F)
+
+table(DEA_genes_F %in% DEA_genes_M)
+table(DEA_genes_M %in% DEA_genes_F)
