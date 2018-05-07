b9b4ad09afb9d4e4650ac5f56c4e35dc531a93a7
Laurent Modolo <laurent.modolo@ens-lyon.fr>
2018-05-07 11:22:24 +0200

    6_in_vitro_cycling.R: merge all infos from M and F donor with in vitro

--- src/6_in_vitro_cycling.R
+++ src/6_in_vitro_cycling.R
@@ -1,11 +1,37 @@
 setwd("~/projects/yellow_fever")
 devtools::load_all("../scRNAtools/", reset = T)
-load(file = "results/cycling/CB_counts_QC_cycling.Rdata")
 
-scd$setfeature(
-  "experiment",
-  gsub("(P\\d+)_\\d+", "\\1", scd$getfeature("id"), perl = T)
-)
+load("results/cycling/CB_counts_QC_cycling.Rdata")
+b_cells <- scd$getfeature("sex") %in% "M" &
+  scd$getfeature("day") %in% c("D15", "D136", "D593")
+infos <- scd$getfeatures
+CB_counts <- scd$getcounts
+load("results/QC/CB_counts_QC.Rdata")
+cells_counts <- scd$getcounts
+
+load("results/cycling/CB_counts_QC_cycling.Rdata")
+b_cells <- scd$getfeature("sex") %in% "F" &
+  scd$getfeature("day") %in% c("D15", "D90")
+infos[b_cells, ] <- scd$select(b_cells = b_cells)$getfeatures
+CB_counts[b_cells, ] <- scd$select(b_cells = b_cells)$getcounts
+load("results/QC/CB_counts_QC_F.Rdata")
+cells_counts[b_cells, ] <- scd$select(b_cells = b_cells)$getcounts
+
+load("results/QC/CB_counts_QC_in_vitro_P1902_P3128.Rdata")
+experiment <- c("P1902", "P3128")
+day <- "InVitro"
+b_cells <- scd$getfeature("day") %in% day &
+  scd$getfeature("experiment") %in% experiment &
+  scd$getfeature("cell_number") %in% 1
+c_cells <- which(colnames(infos) %in% colnames(scd$getfeatures))
+c_cells_bis <- which(colnames(scd$getfeatures) %in% colnames(infos))
+infos[b_cells, c_cells] <- scd$select(b_cells = b_cells)$getfeatures[, c_cells_bis]
+c_cells_bis <- which(colnames(scd$getfeatures) %in%
+  setdiff(colnames(scd$getfeatures), colnames(infos)))
+infos <- cbind(infos, scd$getfeatures[, c_cells_bis])
+CB_counts[b_cells, ] <- scd$select(b_cells = b_cells)$getcounts
+load("results/QC/cells_counts_QC_in_vitro_P1902_P3128.Rdata")
+cells_counts[b_cells, ] <- scd$select(b_cells = b_cells)$getcounts
 
 founder_phenotype <- rep(NA, scd$getncells)
 founder_infos <- read.csv("data/FounderCellP1902_P3128.csv")
@@ -15,17 +41,46 @@ for (i in 1:nrow(founder_infos)) {
     scd$getfeature("clonality") %in% founder_infos$Clone.ID[i]
   founder_phenotype[b_select] <- as.vector(founder_infos$Founder_Type_Simple[i])
 }
-founder_infos$Founder_Type_Simple
-founder_phenotype
+
+scd <- scdata$new(
+  infos = infos,
+  counts = CB_counts
+)
+
 scd$setfeature(
   "founder_phenotype",
   founder_phenotype
 )
+scd$setfeature(
+  "experiment",
+  gsub("(P\\d+)_\\d+", "\\1", scd$getfeature("id"), perl = T)
+)
+
+save(scd, file = "results/cell_type/CB_counts_QC_all.Rdata")
+
+scd <- scdata$new(
+  infos = infos,
+  counts = cells_counts
+)
+
+scd$setfeature(
+  "founder_phenotype",
+  founder_phenotype
+)
+scd$setfeature(
+  "experiment",
+  gsub("(P\\d+)_\\d+", "\\1", scd$getfeature("id"), perl = T)
+)
+
+save(scd, file = "results/cell_type/cells_counts_QC_all.Rdata")
+
+
 
 system("mkdir -p results/cycling/")
 
 # we try to refine the regev cell-cycle genes list
-load(file="results/cycling/regev_genes.RData")
+load(file="results/cycling/regev_genes.RData", v = T)
+load("results/cell_type/CB_counts_QC_all.Rdata")
 
 cycling_score <- scd$getfeature("cycling_score")
 cycling <- scd$getfeature("cycling")
@@ -33,8 +88,8 @@ pcycling <- scd$getfeature("pcycling")
 genes_cycling <- regev_genes
 require("mixtools")
 for (experiment in c("P1902", "P3128")) {
-  time_range <- "InVitro"
-  b_cells <- scd$getfeature("day") %in% time_range &
+  day <- "InVitro"
+  b_cells <- scd$getfeature("day") %in% day &
     scd$getfeature("experiment") %in% experiment &
     scd$getfeature("cell_number") %in% 1
   cycling_score[b_cells] <- pca_loading(
@@ -47,7 +102,6 @@ for (experiment in c("P1902", "P3128")) {
   cycling_score[b_cells] <- log(abs(
     cycling_score[b_cells] - max(cycling_score[b_cells])
   ) + 1)
-  print(summary(cycling_score[b_cells & scd$getfeature("day") %in% "D136"]))
   model <- normalmixEM(
     cycling_score[b_cells],
     lambda = .5,
@@ -60,7 +114,9 @@ scd$setfeature("cycling", cycling)
 scd$setfeature("pcycling", as.vector(pcycling))
 scd$setfeature("cycling_score", as.vector(cycling_score))
 
-save(scd, file = "results/cycling/CB_counts_QC_cycling_invitro_P1902_P3128.Rdata")
+save(scd,
+  file = "results/cycling/CB_counts_QC_cycling_invitro_P1902_P3128.Rdata"
+)
 infos <- scd$getfeatures
 write.csv(
   infos,
