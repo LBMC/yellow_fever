523d65c7ecdf7f57a0cf60e804e6da5939b50a4a
Laurent Modolo <laurent.modolo@ens-lyon.fr>
2018-06-11 10:45:31 +0200

    *_training.R analysis

---
+++ src/8_training_QC.R
@@ -0,0 +1,90 @@
+rm(list=ls())
+setwd("~/projects/yellow_fever/")
+devtools::load_all("../scRNAtools/", reset = T)
+load("results/QC/counts_QC.Rdata")
+scd$setfeature(
+  "experiment",
+  gsub("(P\\d+)_\\d+", "\\1", scd$getfeature("id"), perl = T)
+)
+experiment <- scd$getfeature("experiment")
+experiment[scd$getfeature("day") %in% "NONE"] <- "training"
+scd$setfeature("experiment", experiment)
+
+experiment <- "training"
+b_cells <- scd$getfeature("experiment") %in% experiment &
+  scd$getfeature("cell_number") %in% 1
+
+QC_score <- ifelse(b_cells,
+  0.5,
+  scd$getfeature("QC_score")
+)
+to_QC <- ifelse(b_cells,
+  TRUE,
+  scd$getfeature("to_QC")
+)
+table(b_cells)
+length(scd$getfeature("to_QC"))
+length(QC_score)
+scd$setfeature("QC_score",
+  QC_score
+)
+scd$setfeature("to_QC",
+  to_QC
+)
+scRNAtools::QC_classification(
+  scd = scd,
+  is_blank = scd$getfeature("QC_score") < 0.5
+)
+save(scd, file = paste0("results/QC/counts_QC_", experiment, ".Rdata"))
+print(experiment)
+print(summary(scd$select(b_cells = b_cells)$getfeature("QC_good")))
+print(table(scd$getfeature('sex'), scd$getfeature("QC_good")))
+
+scRNAtools::pca_plot(
+  scd$select(b_cells = b_cells), color = "QC_good", color_name = "antigen",
+  tmp_file = paste0("results/tmp/pca_counts_", experiment, "_tmp.Rdata"),
+  main = "training"
+)
+
+scRNAtools::pca_plot(
+  scd$select(b_cells = b_cells & scd$getfeature("QC_good") %in% T),
+  color = "batch", color_name = "clonality",
+  tmp_file = paste0("results/tmp/pca_counts_QC_", experiment, "_tmp.Rdata"),
+  main = "training"
+)
+
+save(scd, file = "results/QC/counts_QC_training.Rdata")
+
+# cells effect normalization
+load("results/QC/counts_QC_training.Rdata")
+devtools::load_all("../scRNAtools/", reset = T)
+
+experiment <- "training"
+b_cells <- scd$getfeature("experiment") %in% experiment &
+  scd$getfeature("cell_number") %in% 1 &
+    scd$getfeature("QC_good") %in% T
+bad_cells <- scd$
+  select(b_cells = b_cells, genes = ERCC(scd, minus = T))$getcounts
+bad_cells <- rowSums(bad_cells > 5) < 1000
+QC_good <- scd$getfeature("QC_good")
+QC_good[bad_cells] <- FALSE
+scd$setfeature("QC_good", QC_good)
+
+system(paste0("rm results/tmp/normalization_", experiment, "_tmp.Rdata"))
+b_cells <- scd$getfeature("experiment") %in% experiment &
+  scd$getfeature("cell_number") %in% 1 &
+    scd$getfeature("QC_good") %in% T
+scd <- normalize(
+  scd = scd,
+  b_cells = b_cells,
+  method = "SCnorm",
+  cpus = 3,
+  tmp_file = paste0("results/tmp/normalization_", experiment, "_tmp.Rdata")
+)
+save(scd, file = "results/QC/cells_counts_QC_training.Rdata")
+scRNAtools::pca_plot(
+  scd$select(b_cells = b_cells), color = "QC_good", color_name = "antigen",
+  tmp_file = paste0("results/tmp/pca_cells_counts_", experiment, "_tmp.Rdata"),
+  main = "training"
+)
+
---
+++ src/9_training_DEA.R
@@ -0,0 +1,162 @@
+rm(list=ls())
+setwd("~/projects/yellow_fever/")
+devtools::load_all("../scRNAtools/", reset = T)
+load("results/QC/cells_counts_QC_training.Rdata")
+
+experiment <- "training"
+b_cells <- scd$getfeature("experiment") %in% experiment &
+  scd$getfeature("cell_number") %in% 1 &
+  scd$getfeature("QC_good") %in% T
+infos_M <- scd$select(b_cells = b_cells)$getfeatures
+counts_M <- scd$select(b_cells = b_cells)$getcounts
+infos_M <- t(infos_M)
+counts_M <- t(counts_M)
+dim(infos_M)
+dim(counts_M)
+normalized_counts <- rbind(infos_M, counts_M)
+dim(normalized_counts)
+write.csv(
+  normalized_counts,
+  file = paste0("results/cell_type/cells_counts_training.csv")
+)
+
+infos <- scd$getfeatures
+write.csv(
+  infos,
+  file = paste0("results/cell_type/nfos_training.csv")
+)
+
+# single-cells
+experiment <- "training"
+b_cells <- scd$getfeature("experiment") %in% experiment &
+  scd$getfeature("cell_number") %in% 1 &
+  scd$getfeature("QC_good") %in% T
+DEA_cell_type <- scd$getfeature("phenotype_surface_marker")
+levels(DEA_cell_type) <- c(NA, "CM", "CM", "EFF", "EM", "EM", "MEM", "Naive",
+                           "Temra", "Temra", "Temra", "TSCM", "TSCM")
+DEA_cell_type[b_cells & !( DEA_cell_type %in% c("CM", "EM") )] <- NA
+scd$setfeature("DEA_cell_type", DEA_cell_type)
+
+experiment <- "training"
+b_cells <- scd$getfeature("experiment") %in% experiment &
+  scd$getfeature("cell_number") %in% 1 &
+  scd$getfeature("QC_good") %in% T &
+  scd$getfeature("DEA_cell_type") %in% c("CM", "EM")
+system(
+  paste0("rm -R results/cell_type/mbatch_", experiment, "_DEA_cell_type_DEA")
+)
+system(
+  paste0("mkdir -p results/cell_type/mbatch_", experiment, "_DEA_cell_type_DEA")
+)
+mbatch_DEA_cell_type_DEA <- DEA(
+  scd = scd,
+  formula_null = "y ~ 1",
+  formula_full = "y ~ DEA_cell_type",
+  b_cells = b_cells,
+  cpus = 10,
+  v = T,
+  folder_name = paste0("results/cell_type/mbatch_", experiment, "_DEA_cell_type_DEA")
+)
+save(
+  mbatch_DEA_cell_type_DEA,
+  file = paste0("results/cell_type/mbatch_", experiment, "_DEA_cell_type_DEA.Rdata")
+)
+system("~/scripts/sms.sh \"DEA done\"")
+print(table(is.na(mbatch_DEA_cell_type_DEA$padj)))
+print(table(mbatch_DEA_cell_type_DEA$padj < 0.05))
+write.csv(
+  mbatch_DEA_cell_type_DEA,
+  file = paste0("results/cell_type/mbatch_", experiment,
+                "_DEA_cell_type_DEA.csv")
+)
+
+# bulk
+experiment <- "training"
+b_cells <- scd$getfeature("experiment") %in% experiment &
+  !( scd$getfeature("cell_number") %in% 1  )
+DEA_cell_type <- scd$getfeature("phenotype_surface_marker")
+levels(DEA_cell_type) <- c(NA, "CM", "CM", "EFF", "EM", "EM", "MEM", "Naive",
+                           "Temra", "Temra", "Temra", "TSCM", "TSCM")
+DEA_cell_type[b_cells & !( DEA_cell_type %in% c("CM", "EM") )] <- NA
+scd$setfeature("DEA_cell_type", DEA_cell_type)
+
+experiment <- "training"
+b_cells <- scd$getfeature("experiment") %in% experiment &
+  !( scd$getfeature("cell_number") %in% 1  )&
+  scd$getfeature("DEA_cell_type") %in% c("CM", "EM")
+table(b_cells)
+
+library(DESeq2)
+countData <- t(round(
+    scd$select(
+      b_cells = b_cells,
+      genes = ERCC(scd, minus = T))$getcounts))
+colData <- scd$select(b_cells = b_cells)$getfeatures
+
+dds <- DESeqDataSetFromMatrix(
+  countData = countData + 1,
+  colData = colData,
+  design = ~ DEA_cell_type)
+dds <- dds[ rowSums(counts(dds)) > 1, ]
+geoMean <- apply(
+  counts(dds),
+  1,
+  FUN = function(x){
+    if(all(x == 0)) {
+      0
+    } else {
+      exp(mean(log(x[x !=0 ])))
+    }
+  }
+)
+dds <- estimateSizeFactors(dds, geoMean = geoMean)
+dds <- estimateDispersions(dds, fitType = "local")
+dds <- nbinomLRT(dds, reduced = ~1)
+res <- results(dds)
+
+save(res, file = "results/cell_type/bulk_training_cell_type.Rdata")
+write.table(res, file = "results/cell_type/bulk_training_cell_type.csv")
+
+sum(res$padj < 0.05, na.rm=TRUE)
+
+DE_genes <- rownames(res)[res$padj < 0.05 & !is.na(res$padj)]
+
+pca_plot(scd$select(b_cells = b_cells, genes = DE_genes), color="DEA_cell_type",
+  color_name = "clonality", tmp_file = "results/tmp_bulk_training_pca.Rdata")
+ggsave(
+  "results/cell_type/pca/pca_bulk_training_cell_type.pdf",
+  width = 20, height = 15, units = "cm", dpi = 1200
+)
+
+# compaison
+load(
+  file = paste0("results/cell_type/mbatch_", experiment,
+                "_DEA_cell_type_DEA.Rdata")
+)
+save(file = "results/cell_type/bulk_training_cell_type.Rdata")
+DEG_single <- mbatch_DEA_cell_type_DEA$gene[mbatch_DEA_cell_type_DEA$padj < 0.05 &
+                             !is.na( mbatch_DEA_cell_type_DEA$padj )]
+DEG_bulk <- rownames(res)[res$padj < 0.05 & !is.na(res$padj)]
+table(DEG_single %in% DEG_bulk)
+table(DEG_bulk %in% DEG_single)
+
+b_cells <- scd$getfeature("experiment") %in% experiment &
+  scd$getfeature("cell_number") %in% 1 &
+  scd$getfeature("QC_good") %in% T
+DEA_cell_type <- scd$getfeature("phenotype_surface_marker")
+levels(DEA_cell_type) <- c(NA, "CM", "CM", "EFF", "EM", "EM", "MEM", "Naive",
+                           "Temra", "Temra", "Temra", "TSCM", "TSCM")
+DEA_cell_type[b_cells & !( DEA_cell_type %in% c("CM", "EM") )] <- NA
+scd$setfeature("DEA_cell_type", DEA_cell_type)
+experiment <- "training"
+b_cells <- scd$getfeature("experiment") %in% experiment &
+  scd$getfeature("cell_number") %in% 1 &
+  scd$getfeature("QC_good") %in% T &
+  scd$getfeature("DEA_cell_type") %in% c("CM", "EM")
+
+pca_plot(scd$select(b_cells = b_cells, genes = DEG_bulk), color="DEA_cell_type",
+  color_name = "clonality", tmp_file = "results/tmp_scd_training_pca.Rdata")
+ggsave(
+  "results/cell_type/pca/pca_sc_training_cell_type.pdf",
+  width = 20, height = 15, units = "cm", dpi = 1200
+)
