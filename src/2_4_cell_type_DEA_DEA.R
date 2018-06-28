025a9f06dda73cce6888fd25b64c32699dff7d8e
Laurent Modolo <laurent.modolo@ens-lyon.fr>
2018-06-28 13:40:07 +0200

    2_4_cell_type_DEA_DEA.R: add DEA for pDEA_cell_type

--- src/2_4_cell_type_DEA_DEA.R
+++ src/2_4_cell_type_DEA_DEA.R
@@ -46,6 +46,44 @@ for (day in c("D15", "D136", "D593")) {
 
 for (day in c("D15", "D136", "D593")) {
   system(
+    paste0("mkdir -p results/cell_type/mbatch_", day, "_pDEA_cell_type_DEA")
+  )
+  b_cells <- scd$getfeature("QC_good") %in% T &
+    !is.na(scd$getfeature("DEA_cell_type")) &
+    scd$getfeature("day") %in% day
+  mbatch_pDEA_cell_type_DEA <- DEA(
+    scd = scd,
+    formula_null = "y ~ (1|batch)",
+    formula_full = "y ~ (1|batch) + pDEA_cell_type",
+    continuous = "pDEA_cell_type",
+    b_cells = b_cells,
+    cpus = 11,
+    v = T,
+    folder_name = paste0("results/cell_type/mbatch_", day, "_pDEA_cell_type_DEA")
+  )
+  save(
+    mbatch_DEA_cell_type_DEA,
+    file = paste0("results/cell_type/mbatch_", day, "_pDEA_cell_type_DEA.Rdata")
+  )
+  system("~/scripts/sms.sh \"DEA done\"")
+  print(day)
+  print(table(is.na(mbatch_pDEA_cell_type_DEA$padj)))
+  print(table(mbatch_pDEA_cell_type_DEA$padj < 0.05))
+}
+
+for (day in c("D15", "D136", "D593")) {
+  load(paste0("results/cell_type/mbatch_", day, "_pDEA_cell_type_DEA.Rdata"))
+  print(day)
+  print(table(is.na(mbatch_pDEA_cell_type_DEA$padj)))
+  print(table(mbatch_pDEA_cell_type_DEA$padj < 0.05))
+  write.csv(
+    mbatch_pDEA_cell_type_DEA,
+    file = paste0("results/cell_type/mbatch_", day, "_pDEA_cell_type_DEA.csv")
+  )
+}
+
+for (day in c("D15", "D136", "D593")) {
+  system(
     paste0("mkdir -p results/cell_type/mbatch_", day, "_DEA_cell_type_DEA_logit")
   )
   b_cells <- scd$getfeature("QC_good") %in% T &
