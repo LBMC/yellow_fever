70c7b5087659f5af45f281ea2827cfe0d3652cd3
Laurent Modolo <laurent@modolo.fr>
2020-05-13 14:39:31 +0200

    03_cell_type_surface_PLS.R: add PLS classif and DEA on the results

--- src/03_cell_type_surface_PLS.R
+++ src/03_cell_type_surface_PLS.R
@@ -20,11 +20,15 @@ no_surface <- colData(sce)[, genes_PLS %>%
                ] %>%
   as_tibble() %>% rowSums() %>% is.na()
 sce$manual_cell_type[no_surface] <- NA
+genes_PLS %>% pull(proteins) %>% na.omit() %>% unique()
+genes_PLS
 
 fit <- PLS_fit(
   sce = sce,
   group_by = sce$manual_cell_type,
-  genes = genes_PLS %>% pull(genes) %>% na.omit(),
+  genes = rownames(sce)[
+    match(genes_PLS %>% pull(genes) %>% na.omit(),rowData(sce)$gene)
+  ],
   features = genes_PLS %>% pull(proteins) %>% na.omit() %>% unique(),
   assay_name = "counts_vst",
   altExp_name = "PLS_surface_cell_type",
@@ -37,7 +41,9 @@ sce <- PLS_predict(
   sce = fit$sce,
   fit = fit,
   group_by = sce$manual_cell_type,
-  genes = genes_PLS %>% pull(genes) %>% na.omit(),
+  genes = rownames(sce)[
+    match(genes_PLS %>% pull(genes) %>% na.omit(),rowData(sce)$gene)
+  ],
   features = genes_PLS %>% pull(proteins) %>% na.omit() %>% unique(),
   assay_name = "counts_vst",
   altExp_name = "PLS_surface_cell_type",
@@ -45,6 +51,13 @@ sce <- PLS_predict(
 )
 save(sce, file = "results/sce_PLS_surface_cell_type.Rdata")
 load(file = "results/sce_PLS_surface_cell_type.Rdata", verbose = T)
+
+altExp(sce, "PLS_surface_cell_type") %>% 
+  rowData() %>% 
+  as_tibble(rownames = "id") %>% 
+  select(id, predictor) %>% 
+  filter(predictor) %>% 
+  left_join(rowData(sce) %>% as_tibble())
 
 colData(sce) %>% 
   as_tibble() %>% 
@@ -65,12 +78,129 @@ DEA_surface_cell_type <- DEA(
   assay_name = "counts_vst",
   cpus = 10
 )
-
-save(fit, file = "results/03_DEA.Rdata")
+save(DEA_surface_cell_type, file = "results/03_DEA.Rdata")
 load(file = "results/03_DEA.Rdata", verbose = T)
 
-rowData(sce)$p_PLS_surface_cell_type_male_invivo <- NA
-rowData(sce)$p_PLS_surface_cell_type_male_invivo[sce$male_invivo] <- 
+assay(sce[
+    match(genes_PLS %>% pull(genes) %>% na.omit(),rowData(sce)$gene),
+    sce$male_invivo
+  ],
+  "counts_vst") %>% 
+  t() %>% 
+  as.matrix() %>% 
+  as_tibble() %>% 
+  pivot_longer(cols = everything()) %>% 
+  left_join(rowData(sce) %>% 
+              as_tibble() %>% 
+              dplyr::rename(name = id)) %>% 
+  ggplot() +
+  geom_histogram(aes(x = value)) +
+  facet_wrap(~gene)
+
+rowData(sce)$pval_DEA_p_PLS_surface_cell_type_male_invivo <- NA
+rowData(sce)$pval_DEA_p_PLS_surface_cell_type_male_invivo <- 
   get_genes_pval(DEA_surface_cell_type)
 
+rowData(sce)$pval_DEA_p_PLS_surface_cell_type_male_invivo %>% 
+  is.na() %>% 
+  table()
+
+DEA_surface_cell_type_fixed <- DEA(
+  sce[
+    rowData(sce)$pval_DEA_p_PLS_surface_cell_type_male_invivo %>% is.na(),
+    sce$male_invivo
+  ],
+  test = "~ p_PLS_surface_cell_type",
+  formula = "count ~ p_PLS_surface_cell_type + day + batch",
+  assay_name = "counts_vst",
+  cpus = 10
+)
+
+save(DEA_surface_cell_type_fixed, file = "results/03_DEA_fixed.Rdata")
+
+get_genes_pval(DEA_surface_cell_type_fixed) %>%
+  is.na() %>%
+  table()
+
+rowData(sce)$pval_DEA_p_PLS_surface_cell_type_male_invivo[
+  rowData(sce)$pval_DEA_p_PLS_surface_cell_type_male_invivo %>% is.na()
+] <- get_genes_pval(DEA_surface_cell_type_fixed)
+
+rowData(sce)$pval_DEA_p_PLS_surface_cell_type_male_invivo %>% 
+  is.na() %>% 
+  table()
+
+rowData(sce)$pval_DEA_p_PLS_surface_cell_type_male_invivo_adj <- p.adjust(
+  rowData(sce)$pval_DEA_p_PLS_surface_cell_type_male_invivo,
+  method = "BH"
+)
+
+rowData(sce) %>% 
+  as_tibble() %>% 
+  ggplot(aes(log_mean, log_var, color = is.na(pval_DEA_p_PLS_surface_cell_type_male_invivo_adj))) +
+  geom_point(alpha = 0.3, shape = 16) +
+  geom_density_2d(size = 0.3) +
+  geom_abline(intercept = 0,
+              slope = 1,
+              color = 'red')
+
 save(sce, file = "results/sce_DEA_surface_cell_type.Rdata")
+load(file = "results/sce_DEA_surface_cell_type.Rdata")
+
+sce <- logNormCounts(sce, exprs_values = "counts_vst")
+sce_DEA <- runPCA(sce[
+    rowData(sce)$pval_DEA_p_PLS_surface_cell_type_male_invivo_adj <= 0.05 &
+      !is.na(rowData(sce)$pval_DEA_p_PLS_surface_cell_type_male_invivo_adj), 
+    colData(sce)$male_invivo & !is.na(colData(sce)$p_PLS_surface_cell_type)
+  ])
+sce_DEA <- runTSNE(sce_DEA)
+
+plotPCA(sce_DEA,
+  colour_by = "p_PLS_surface_cell_type")
+plotTSNE(sce_DEA,
+  colour_by = "p_PLS_surface_cell_type")
+plotPCA(sce_DEA,
+  colour_by = "manual_cell_type")
+plotTSNE(sce_DEA,
+  colour_by = "manual_cell_type")
+plotPCA(sce_DEA,
+  colour_by = "day")
+plotTSNE(sce_DEA,
+  colour_by = "day")
+
+rownames(sce_DEA) <- rowData(sce_DEA)$gene
+plotHeatmap(
+  sce_DEA,
+  features = rownames(sce_DEA) %in% (
+    rownames(sce_DEA)[order(rowData(sce_DEA)$pval_DEA_p_PLS_surface_cell_type_male_invivo_adj)] %>% head(30)
+  ),
+  order_columns_by = "p_PLS_surface_cell_type",
+  order_rows_by = "pval_DEA_p_PLS_surface_cell_type_male_invivo_adj",
+  colour_columns_by = c("day", "antigen", "manual_cell_type"),
+  center = TRUE,
+  symmetric = TRUE,
+  zlim = c(-5, 5)
+) 
+
+plotHeatmap(
+  sce_DEA,
+  features = rownames(sce_DEA) %in% (
+    genes_PLS %>% pull(genes) %>% na.omit()   
+  ),
+  order_columns_by = "p_PLS_surface_cell_type",
+  order_rows_by = "pval_DEA_p_PLS_surface_cell_type_male_invivo_adj",
+  colour_columns_by = c("day", "antigen", "manual_cell_type"),
+  center = TRUE,
+  symmetric = TRUE,
+  zlim = c(-5, 5)
+) 
+
+rowData(sce[
+  rowData(sce)$gene %in% (genes_PLS %>% pull(genes) %>% na.omit()),
+  ]
+)
+
+rowData(sce[
+  rowData(sce)$gene %in% (genes_PLS %>% pull(genes) %>% na.omit()),
+  ]
+)
