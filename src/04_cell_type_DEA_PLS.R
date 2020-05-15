07b830cae05c3f7051e8bd8ee6c675f8182465f4
Laurent Modolo <laurent@modolo.fr>
2020-05-15 11:52:58 +0200

    04_cell_type_DEA_PLS.R: add diagnostic plots

--- src/04_cell_type_DEA_PLS.R
+++ src/04_cell_type_DEA_PLS.R
@@ -1,3 +1,4 @@
+rm(list = ls())
 source("src/00_functions.R")
 
 load(file = "results/sce_DEA_surface_cell_type.Rdata", verbose = T)
@@ -56,6 +57,10 @@ colData(sce) %>%
   filter(male_invivo) %>%
   summary()
 
+colData(sce) %>%
+  as_tibble() %>%
+  filter(YVF2003_D1401) %>%
+  summary()
 sce[rownames(sce) %in% (
     altExp(fit$sce, "PLS_DEA_cell_type") %>%
       assay("counts") %>% 
@@ -89,8 +94,6 @@ altExp(sce, "PLS_DEA_cell_type") %>%
 colData(sce) %>%
   as_tibble() %>%
   filter(male_invivo) %>%
-  summary()
-  
   ggplot() +
   geom_point(aes(x = p_PLS_DEA_cell_type, y = sum, color = PLS_DEA_cell_type)) +
   facet_wrap(~day) +
@@ -149,26 +152,15 @@ rowData(sce)$pval_DEA_p_PLS_DEA_cell_type_male_invivo_adj <- p.adjust(
   method = "BH"
 )
 
-colData(sce) %>% 
-  as_tibble() %>% 
-  filter(male_invivo) %>% 
-  ggplot() +
-  geom_point(aes(x = p_PLS_DEA_cell_type, y = sum, color = PLS_DEA_cell_type)) +
-  facet_wrap(~day) +
-  scale_y_log10() +
-  theme_bw() +
-  annotation_logticks() +
-  labs(y = "genes detected",
-       x = " sum")
 
 save(sce, file = "results/sce_DEA_DEA_cell_type.Rdata")
 load(file = "results/sce_DEA_DEA_cell_type.Rdata")
 
 sce <- logNormCounts(sce, exprs_values = "counts_vst")
 sce_DEA <- runPCA(sce[
-    rowData(sce)$pval_DEA_PLS_DEA_cell_type_male_invivo_adj <= 0.05 &
-      !is.na(rowData(sce)$pval_DEA_DEA_surface_cell_type_male_invivo_adj), 
-    colData(sce)$male_invivo
+    rowData(sce)$pval_DEA_p_PLS_DEA_cell_type_male_invivo_adj <= 0.05 &
+      !is.na(rowData(sce)$pval_DEA_p_PLS_DEA_cell_type_male_invivo_adj), 
+    sce$male_invivo
   ])
 sce_DEA <- runTSNE(sce_DEA)
 
@@ -182,5 +174,90 @@ plotTSNE(sce_DEA,
   colour_by = "manual_cell_type")
 plotPCA(sce_DEA,
   colour_by = "day")
-plotTSNE(sce_DEA,
-  colour_by = "day")
+
+colData(sce) %>% 
+  as_tibble() %>% 
+  mutate(day = fct_relevel(day, "D15", "D90", "D136", "D593", "D1401")) %>%  
+  filter(!is.na(ccr7), !is.na(il7ra)) %>% 
+  ggplot(aes(
+      x = ccr7,
+      y = il7ra,
+  )) +
+  scale_color_manual(values=c("#0571b0", "#c90000")) +
+  geom_point(aes(color = manual_cell_type), size = 2) +
+  ggnewscale::new_scale_color() +
+  scale_colour_gradient2(low = "blue", mid = "gray", high = "red",
+            midpoint = 0.5) +
+  geom_point(aes(color = p_PLS_DEA_cell_type), size = 1) +
+  facet_wrap(~sex + day, ncol = 3) +
+  scale_x_log10() +
+  scale_y_log10() +
+  theme_bw() +
+  annotation_logticks() +
+  labs(y = "il7ra",
+       x = "ccr7")
+
+colData(sce_DEA) %>% 
+  as_tibble() %>% 
+  left_join(
+    reducedDim(sce_DEA, "TSNE") %>%
+      as_tibble(rownames = "id")
+    ) %>% 
+  mutate(day = fct_relevel(day, "D15", "D136", "D593")) %>% 
+  rename(c("V1" = "TSNE1", "V2" = "TSNE2")) %>% 
+  pivot_longer(cols = c("p_PLS_DEA_cell_type", "p_PLS_surface_cell_type"),
+               names_to = "PLS_type",
+               values_to = "PLS_proba") %>%  
+  mutate(PLS_type = fct_relevel(PLS_type, "p_PLS_surface_cell_type", "p_PLS_DEA_cell_type")) %>% 
+  ggplot(aes(
+      x = TSNE1,
+      y = TSNE2,
+  )) +
+  scale_color_manual(values=c("#0571b0", "#c90000")) +
+  labs(color = "manual cell-type") +
+  geom_point(aes(color = manual_cell_type), size = 2) +
+  ggnewscale::new_scale_color() +
+  scale_colour_gradient2(low = "blue", mid = "gray", high = "red",
+            midpoint = 0.5) +
+  labs(color = "PLS cell-type probability") +
+  geom_point(aes(color = PLS_proba), size = 1) +
+  facet_wrap(~PLS_type + sex + day, ncol = 3) +
+  theme_bw() +
+  labs(y = "t-SNE1",
+       x = "t-SNE2")
+
+sce_hm <- sce
+
+sce_DEA_hm <- sce[
+  rowData(sce_DEA_hm)$gene %in% c(
+    "GZMB",
+    "CX3CR1",
+    "CCL4",
+    "GNLY",
+    "GZMH",
+    "KLRD1",
+    "GZMG",
+    "PRF1",
+    "HOPX",
+    "CCL5",
+    "GZMK",
+    "SELL",
+    "IL7R",
+    "LEF1",
+    "TCF7",
+    "LTB",
+    "NELL2",
+    "CCR7"
+  ),
+  sce$male_invivo]
+rownames(sce_DEA_hm) <- rowData(sce_DEA_hm)$gene
+
+plotHeatmap(
+  sce_DEA_hm,
+  features = rownames(sce_DEA_hm),
+  order_columns_by = "p_PLS_DEA_cell_type",
+  colour_columns_by = c("p_PLS_DEA_cell_type", "manual_cell_type", "day"),
+  center = TRUE,
+  symmetric = TRUE,
+  zlim = c(-5, 5)
+) 
